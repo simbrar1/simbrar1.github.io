5b35d5b2b09512bd10cd10136f01505d2aec5f16
simbrar1 <sim.brar1@hotmail.com>
2019-09-25 08:47:12 +0100

    modal

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,10 @@ function change(x) {
   x.classList.toggle('change')
 }
 
+function change(x) {
+  x.classList.toggle('change')
+}
+
 // Get the modal
 var modal = document.getElementById('myModal')
 
@@ -14,16 +18,19 @@ var span = document.getElementsByClassName('close')[0]
 // When the user clicks on the button, open the modal 
 btn.onclick = function () {
   modal.style.display = 'block';
+  modal.style.opacity = 1;
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
   modal.style.display = 'none';
+  modal.style.opacity = 0;
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = 'none';
+    modal.style.opacity = 0;
   }
 }
